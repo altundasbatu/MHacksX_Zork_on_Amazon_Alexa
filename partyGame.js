c26b3e8849f4159912864a7174cc235a1c977be8
Cem Akdemir <cem.akd@hotmail.com>
2017-09-23 20:23:35 -0400

    No intent added

--- partyGame.js
+++ partyGame.js
@@ -6,7 +6,7 @@ exports.handler = (event, context) => {
     var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
     var soloActions = ["Do 5 push ups",
                        "Do 10 push ups",
-                       "Drink with anyone you choose!",
+                       "Drink with anyone they choose!",
                        "Drink!",
                        "You can't speak for three rounds",
                        "Pick a person to mute",
@@ -28,11 +28,9 @@ exports.handler = (event, context) => {
         console.log(`LAUNCH REQUEST`);
         context.succeed(
           generateResponse(
-            buildSpeechletResponse("Welcome to Cardy Party Game! Say help if it's your first time.", false)
+            buildSpeechletResponse("Welcome to Cardy Party Game! Say help if it's your first time. Are you ready to play?", false)
           )
         );
-
-        buildReprompt(context, "Are you ready to play?");
         break;
 
       case "IntentRequest":
@@ -41,29 +39,17 @@ exports.handler = (event, context) => {
 
         switch(event.request.intent.name) {
           case "AMAZON.YesIntent":
-            if(getRandomInt(0,1) > 0.5){
-              var randomRankIndex = getRandomInt(0, 13);
-              var randomSuitIndex = getRandomInt(0, 4);
+            if(getRandomInt(0,2) >= 1){
               var actionIndex = getRandomInt(0, 8);
-              var randomRank = ranks[randomRankIndex];
-              var randomSuit = suits[randomSuitIndex];
-              buildResponse(context, "I choose whoever has " + randomRank + " of " + randomSuit + "to " + soloActions[actionIndex], false);
+              buildResponse(context, "I choose whoever has, " + getRandomCard(suits, ranks) + "to, " + soloActions[actionIndex], false);
             }
             else{
-              var randomRankIndex1 = getRandomInt(0, 13);
-              var randomSuitIndex1 = getRandomInt(0, 4);
               var actionIndex = getRandomInt(0, 3); 
-              var randomRank1 = ranks[randomRankIndex1];
-              var randomSuit1 = suits[randomSuitIndex1];
-              var randomRankIndex2 = getRandomInt(0, 13);
-              var randomSuitIndex2 = getRandomInt(0, 4);
-              var randomRank2 = ranks[randomRankIndex2];
-              var randomSuit2 = suits[randomSuitIndex2];
-              buildResponse(context, "I choose whoever has " + randomRank1 + " of " + randomSuit1 + "to " + groupActions[actionIndex] + randomRank2 + " of " + randomSuit2, false);
+              buildResponse(context, "I choose whoever has, " + getRandomCard(suits, ranks) + "to, " + groupActions[actionIndex] + getRandomCard(suits, ranks), false);
             }
             break;
           case "AMAZON.NoIntent":
-            buildResponse(context, "I'll be waiting. Say yes when you are ready!", true);
+            buildResponse(context, "Launch me again when you are ready!", true);
           case "AMAZON.HelpIntent":
             buildResponse(context, "Deal an entire deck of cards to all players. When you are ready say, launch Party Game and follow the instructions. Have fun!", true);
             break;
@@ -138,7 +124,11 @@ buildReprompt = (context, output) => {
   );
 }
 
-
+function getRandomCard(suits, ranks) {
+  var randomRankIndex = getRandomInt(0, ranks.length);
+  var randomSuitIndex = getRandomInt(0, suits.length);
+  return ranks[randomRankIndex]; + " of " + suits[randomSuitIndex];;
+}
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
